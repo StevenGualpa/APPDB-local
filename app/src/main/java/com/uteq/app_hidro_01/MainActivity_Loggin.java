de1d280731e4d9d20b56c574486bfdaab178b055
Steven Gualpa <68717276+StevenGualpa@users.noreply.github.com>
2023-04-30 22:02:54 -0500

    version 04

---
+++ app/src/main/java/com/uteq/app_hidro_01/MainActivity_Loggin.java
@@ -0,0 +1,14 @@
+package com.uteq.app_hidro_01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity_Loggin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_loggin2);
+    }
+}
